7406eb93e5c580bbe3317944d9512a924c7df328
Mike Paiva <mikepaiva@Mac-mini.local>
2021-03-11 19:48:35 -0500

    gmail login added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import smtplib
+import getpass
+
+smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
+smtp_object.ehlo()
+smtp_object.starttls()
+
+email = getpass.getpass("Email address: ")
+password = getpass.getpass("Password: ")  # best practice to generate an app password when testing
+
+smtp_object.login(email, password)
 
 
